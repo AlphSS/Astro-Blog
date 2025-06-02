d180956a64d0452ea64d32af56cd78505fed8f91
Niraj Shah <nirajshah3164@gmail.com>
2025-06-02 09:50:26 +0530

    #

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,8 +1,8 @@
 export default function Video() {
   return (
     <button
-      className="text-amber-800 rounded-3xl bg-amber-400 h-15 w-full"
-      onClick={() => alert("JSX button clicked client side load")}
+      className="text-white rounded-3xl bg-amber-900 h-15 w-full hover:bg-amber-300"
+      onClick={() => alert("JSX button clicked on  client side load")}
     >
       Click Me
     </button>
